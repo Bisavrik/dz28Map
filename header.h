59abdbf799cddc57d8f3b394bbbc223c1705cfb5
Sofia <sofia.apple.2017@icloud.com>
2024-12-14 10:25:33 +0100

    first commit

---
+++ header.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> readFile(const string& filename);
+map<string, int> buildFrequencyDictionary(const vector<string>& lines);
+void printWordFrequencies(const map<string, int>& wordFreq);
+pair<string, int> getMostFrequentWord(const map<string, int>& wordFreq);
+void writeFrequenciesToFile(const map<string, int>& wordFreq, const string& outputFile);
+
+#endif
